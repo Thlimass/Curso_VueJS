a5b2081ba0b8b5a2836ce25b720e93cf5f3ae9a8
Thiago Souza <thlima.silva.souza@gmail.com>
2020-08-19 10:25:39 -0300

    exercicio 04/desafio - exibir img com diretiva v-bind

--- desafio-01/app.js
+++ desafio-01/app.js
@@ -3,6 +3,7 @@ new Vue({
     data: {
         nome: 'Thiago Lima',
         idade: 30,
+        imagem:'https://i.pinimg.com/originals/2e/99/73/2e99730fef279abd4f3f212fd639f546.jpg',
     },
     methods: {
         idadeVezes3(){
